fe367e35c2c4cc62120f790646c48765dc97b7da
soochan <tncksdl05@gmail.com>
2023-05-08 14:29:31 +0900

    init commit

---
+++ src/test/java/com/ecr/ecs/EcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecr.ecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
